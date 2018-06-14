40cc4146fc47df5ef8868b0aba326afd8eafb776
Kevin Chi <kevinchi118@gmail.com>
2018-06-14 19:43:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,6 @@ def key_for_min_value(name_hash)
     name_hash.each do |name, value|
         numbers << value
     end
-    if numbers = []
-        return nil
-    end 
     lowest_value = numbers[0]
     name_hash.each do |name, value|
         if value < lowest_value
